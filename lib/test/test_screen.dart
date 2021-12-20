bb6e649ec7109218ab0bd656ae951f6b8058cfcb
youyoung00 <you889@hanmail.net>
2021-12-20 11:08:38 +0900

    futureBuilder 사용

--- lib/test/test_screen.dart
+++ lib/test/test_screen.dart
@@ -11,14 +11,14 @@ class TestScreen extends StatefulWidget {
 }
 
 class _TestScreenState extends State<TestScreen> {
-  Album? _album;
-
-  Future<void> init() async {
-    Album album = await fetchAlbum();
-    setState(() {
-      _album = album;
-    });
-  }
+  // Album? _album;
+  //
+  // Future<void> init() async {
+  //   Album album = await fetchAlbum();
+  //   setState(() {
+  //     _album = album;
+  //   });
+  // }
 
   Future<Album> fetchAlbum() async {
     final response = await http
@@ -40,17 +40,19 @@ class _TestScreenState extends State<TestScreen> {
   //   return http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
   // }
 
-  @override
-  void initState() {
-    super.initState();
-
-    init();
+  // future builder 를 사용할 경우, initState 를 없애도 됨.
+  // @override
+  // void initState() {
+  //   super.initState();
+  //
+  //   init();
+  // }
     // fetchAlbum().then((album) {
     //   setState(() {
     //     _album = album;
     //   });
     // });
-  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -58,12 +60,39 @@ class _TestScreenState extends State<TestScreen> {
       appBar: AppBar(
         title: const Text("Network Sample"),
       ),
-      body: _album == null
-          ? const Center(child: CircularProgressIndicator())
-          : Text(
-        '${_album!.id} : ${_album.toString()}',
-        style: const TextStyle(fontSize: 30),
+      body: FutureBuilder<Album>(
+        future: fetchAlbum(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(child: Text('네트워크 에러!'));
+          }
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (!snapshot.hasData) {
+            return const Center(child: Text('데이터가 없습니다!'));
+          }
+
+          final Album album = snapshot.data!;
+
+          return _buildBody(album);
+        },
       ),
+      // body: _album == null
+      //     ? const Center(child: CircularProgressIndicator())
+      //     : Text(
+      //   '${_album!.id} : ${_album.toString()}',
+      //   style: const TextStyle(fontSize: 30),
+      // ),
+    );
+  }
+
+  Widget _buildBody(Album album){
+    return  Text(
+      // '${album.id} : ${_album.toString()}',
+      '${album.id} : ${album.toString()}',
+      style: const TextStyle(fontSize: 30),
     );
   }
 }
